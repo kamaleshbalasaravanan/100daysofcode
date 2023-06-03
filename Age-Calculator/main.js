766dc38e40ae4431a67cc11a8633dccdfac9d597
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-03 14:27:50 +0530

    function calculateAge ->created

---
+++ Age-Calculator/main.js
@@ -0,0 +1,68 @@
+// const btnEl = document.getElementById("btn");
+// const birthdayEl = document.getElementById("date");
+// const resultEl = document.getElementById("result");
+
+// function calculateAge(e) {
+//     e.preventDefault()
+//   const birthdayValue = birthdayEl.value;
+//   if (birthdayValue === "") {
+//     alert("Please enter your birthday");
+//   } else {
+//     const age = getAge(birthdayValue);
+//     resultEl.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
+//   }
+// }
+
+// function getAge(birthdayValue) {
+//   const currentDate = new Date();
+//   const birthdayDate = new Date(birthdayValue);
+//   let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+//   const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+//   if (
+//     month < 0 ||
+//     (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+//   ) {
+//     age--;
+//   }
+//   return age;
+// }
+
+// btnEl.addEventListener("click", calculateAge);
+
+const btn = document.querySelector('#btn')
+const result = document.querySelector('#result')
+const date = document.querySelector('#bdate')
+
+btn.addEventListener("click", calculateDate)
+
+function calculateDate(e){
+    e.preventDefault()
+    const birthdayValue= date.value
+    if(birthdayValue === ""){
+        console.log('hi')
+        alert('please enter your DOB')
+    }
+    else{
+        console.log('bye')
+        const age = calculateAge(birthdayValue)
+        result.textContent = `your age is ${age} ${age<=1?"year":"years"}`
+    }
+}
+
+function calculateAge(birthdayValue){
+    console.log("age")
+    const currentDate = new Date()
+    const birthdayDate =new Date(birthdayValue)
+    console.log(birthdayDate.getFullYear())
+    console.log(birthdayDate.getMonth())
+
+    const currentAge = currentDate.getFullYear() - birthdayDate.getFullYear()
+    const month = currentDate.getMonth() - birthdayDate.getMonth()
+    console.log(currentAge+", "+month)
+
+    if( month < 0 || (month === 0 &&  currentDate.getDate() < birthdayDate.getDate()) ){
+        currentAge--
+    }
+        return currentAge
+}
