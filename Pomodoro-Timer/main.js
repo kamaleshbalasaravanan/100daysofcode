ebf25f3855b331b05df2511567b29faa950d7c01
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-05 22:15:15 +0530

    add style for border and heading

--- Pomodoro-Timer/main.js
+++ Pomodoro-Timer/main.js
@@ -4,29 +4,32 @@ const startbtn = document.querySelector('#startbtn')
 const pausebtn = document.querySelector('#pausebtn')
 const restartbtn = document.querySelector('#restartbtn')
 // const container = document.querySelector('.container')
-let container = document.querySelector('.container')
+const container = document.querySelector('.container')
+const heading = document.querySelector('.heading')
 let intervals;
 let timeleft = 1500
 // console.log('hi')
 
 startbtn.addEventListener("click", startTimer)
-pausebtn.addEventListener("click", stopTimer)
+pausebtn.addEventListener("click", pauseTimer)
 restartbtn.addEventListener("click", restartTimer)
 function updateTimer(){
     let minutes = Math.floor(timeleft/60)
     let seconds = timeleft%60
 
     let newtime = `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`
-    console.log(newtime)
+    // console.log(newtime)
     timer.innerHTML = newtime
 }
 
 
 function startTimer(){
 
-       
-        console.log(container)
+       console.log("start")
+        // console.log(container)
         container.style.border = "4px solid green"
+        timer.style.color = "green"
+        heading.style.color = "green"
 
         intervals = setInterval(()=>{
             timeleft--
@@ -44,14 +47,21 @@ function startTimer(){
     
 }
 
-function stopTimer(){
+function pauseTimer(){
+    console.log("pause")
     clearInterval(intervals)
     container.style.border = "4px solid red"
+    timer.style.color = "red"
+    heading.style.color = "red"
+    
 
 }
 
 function restartTimer(){
+    console.log("restart")
     container.style.border = "4px solid black"
+    timer.style.color = "black"
+    heading.style.color = "black"
     clearInterval(intervals)
     timeleft = 1500
     updateTimer()
