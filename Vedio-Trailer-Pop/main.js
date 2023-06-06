2a839132cb37965223e56dd4f4332ca7fe75254c
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-07 03:02:09 +0530

    btn->func

--- Vedio-Trailer-Pop/main.js
+++ Vedio-Trailer-Pop/main.js
@@ -1,1 +1,15 @@
-console.log('hi')
+// console.log('hi')
+const btn = document.querySelector('.btn')
+const close = document.querySelector('.close-icon')
+const trailercontent = document.querySelector('.trailer-content')
+const video = document.querySelectorAll('vedio')
+
+btn.addEventListener("click", ()=>{
+    trailercontent.classList.remove("active")
+})
+
+close.addEventListener("click" , ()=>{
+    trailercontent.classList.add("active")
+    video.pause()
+    video.currentTime = 0
+})
