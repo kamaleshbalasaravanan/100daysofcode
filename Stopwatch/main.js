6e8ea2c3fa9f0b46300912c82de4a9a617244edd
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-06 12:24:36 +0530

    initializing variables

---
+++ Stopwatch/main.js
@@ -0,0 +1,9 @@
+const startBtn =  document.querySelector('.startBtn')
+const stopBtn =  document.querySelector('.stopBtn')
+const restarttBtn =  document.querySelector('.restarttBtn')
+
+const timer = document.querySelector('.timer')
+
+console.log('hi')
+
+
