ca6c89d7a6d9ad20ac939c426db70e54bbe45656
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-13 13:15:00 +0530

    initial commit

---
+++ Character-Counter/main.js
@@ -0,0 +1,8 @@
+const inputarea = document.getElementById('inputfield')
+let len = inputarea.length
+
+console.log(inputarea.length)
+
+inputarea.addEventListener("input", () => {
+    // const valuelen = inputarea.value
+})
