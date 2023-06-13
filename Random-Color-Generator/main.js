31c7f0ca15aaa9380dac6b72d7c62906face00b0
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-13 11:54:11 +0530

    func copyColor

--- Random-Color-Generator/main.js
+++ Random-Color-Generator/main.js
@@ -14,20 +14,40 @@ let generatePalette = () => {
     for(let i=0; i<32; i++){
         let randomHex = Math.floor(Math.random() * 0xffffff).toString(16)
         randomHex = `#${randomHex.padStart(6,"0")}`
-        console.log(randomHex)
+        // console.log(randomHex)
 
         const color = document.createElement('li')
         color.classList.add('color')
         color.innerHTML = ` <div class="rect-box" style = "background-color:${randomHex}"></div>
                             <span class="hex-value">${randomHex}</span>`
+        
+        color.addEventListener("click",  () => copyColor(color ,randomHex))
         colorContainer.appendChild(color)
+
     }
 
-    let randomHex = Math.floor(Math.random() * 0xffffff).toString(16)
-    randomHex = `#${randomHex.padStart(6,"0")}`
-    console.log(randomHex)
+    // let randomHex = Math.floor(Math.random() * 0xffffff).toString(16)
+    // randomHex = `#${randomHex.padStart(6,"0")}`
+    // console.log(randomHex)
+
 }
 
-generatePalette()
 
+function copyColor(element, value){
+    const colorElement = element.querySelector('.hex-value')
+    navigator.clipboard.writeText(value).then(() => {
+        colorElement.innerHTML = "copied"
+    
+    // colorElement.innerHTML.style.text-transform 
+    colorElement.style.fontSize = "20px"
+    colorElement.style.marginTop = "6px"
+
+
+    setTimeout(() => colorElement.innerHTML =value,1000)
+    }).catch(() => alert("failed to copy the color code"))
+    console.log(element,value)
+}
+
+
+generatePalette()
 button.addEventListener("click", generatePalette)
