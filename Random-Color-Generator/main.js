42aa53cadf88ba0a7548798188cc6c03e67d2b5d
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-13 11:13:40 +0530

    func generatePalette

--- Random-Color-Generator/main.js
+++ Random-Color-Generator/main.js
@@ -1,2 +1,33 @@
 let num = 12
-console.log(num.toString())
+const button = document.querySelector('#btn')
+const colorContainer = document.querySelector('.color-container')
+
+
+// const  generatePalette = () => {
+//     let randomhex = Math.floor(Math.random() * 0xffffff).toString(16)
+//     randomhex = `${randomhex.padStart(6,"0")}`
+//     console.log(randomhex)
+// }
+
+let generatePalette = () => {
+    colorContainer.innerHTML = ""
+    for(let i=0; i<32; i++){
+        let randomHex = Math.floor(Math.random() * 0xffffff).toString(16)
+        randomHex = `#${randomHex.padStart(6,"0")}`
+        console.log(randomHex)
+
+        const color = document.createElement('li')
+        color.classList.add('color')
+        color.innerHTML = ` <div class="rect-box" style = "background-color:${randomHex}"></div>
+                            <span class="hex-value">${randomHex}</span>`
+        colorContainer.appendChild(color)
+    }
+
+    let randomHex = Math.floor(Math.random() * 0xffffff).toString(16)
+    randomHex = `#${randomHex.padStart(6,"0")}`
+    console.log(randomHex)
+}
+
+generatePalette()
+
+button.addEventListener("click", generatePalette)
