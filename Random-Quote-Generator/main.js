52a2ccf66faa59d0c6752e16a33f52abd2e8ce41
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-14 22:01:18 +0530

    copyText func

--- Random-Quote-Generator/main.js
+++ Random-Quote-Generator/main.js
@@ -2,6 +2,7 @@
 const quotes = document.querySelector('.container')
 const author = document.querySelector('.author')
 const btn = document.querySelector('#btn')
+const copied = document.querySelector('.copied')
 
 const url = "https://api.quotable.io/random"
 
@@ -9,21 +10,40 @@ let getQuote = () =>{
     fetch(url)
     .then((data) => data.json())
     .then((msg) => {
-        console.log(msg)
-        console.log(msg.content)
-        console.log(msg.author)
+        // console.log(msg)
+        // console.log(msg.content)
+        // console.log(msg.author)
 
 
         // setTimeout( () =>{
         //     quotes.innerHTML = msg.content
         //     author.innerHTML = `" ${msg.author} "`
         // },1000)
-
+        
              quotes.innerHTML = msg.content
             author.innerHTML = `" ${msg.author} "`
-      
-
+            copied.addEventListener("click", () => copyText(msg.content))
     })
+
+    // copied.addEventListener("click", () => copyColor(content, authorname)) 
+  
+    // console.log(content) 
+
+}
+
+function copyText(value){
+    navigator.clipboard.writeText(value).then(() => {
+        quotes.innerHTML = "#copied"
+        quotes.style.fontSize = "40px"
+
+        setTimeout(() => {
+        quotes.innerHTML = value
+        quotes.style.fontSize = "25px"
+        },1000)
+    })
+    // console.log(value)
+
+   
 }
 
 window.addEventListener("load",getQuote)
