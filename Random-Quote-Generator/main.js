868b9832ea54817e6217751d723bcdc56d0c9bfb
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-14 20:07:37 +0530

    func getQuote

--- Random-Quote-Generator/main.js
+++ Random-Quote-Generator/main.js
@@ -1,1 +1,23 @@
-console.log('hi')
+// console.log('hi')
+const quotes = document.querySelector('.container')
+const author = document.querySelector('.author')
+const btn = document.querySelector('#btn')
+
+const url = "https://api.quotable.io/random"
+
+let getQuote = () =>{
+    fetch(url)
+    .then((data) => data.json())
+    .then((msg) => {
+        console.log(msg)
+        console.log(msg.content)
+        console.log(msg.author)
+
+        quotes.innerHTML = msg.content
+        author.innerHTML = `--${msg.author}--`
+
+    })
+}
+
+window.addEventListener("load",getQuote)
+btn.addEventListener("click",getQuote)
