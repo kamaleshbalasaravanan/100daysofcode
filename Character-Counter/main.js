ffd6aae447d6565b3e858aea6eec20e102543179
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-15 12:51:02 +0530

    add if

--- Character-Counter/main.js
+++ Character-Counter/main.js
@@ -1,7 +1,19 @@
 const textcontent = document.querySelector('#myText')
 const result = document.querySelector('#result')
+const remaining = document.querySelector('#remaining')
+
 
 textcontent.addEventListener("input", () => {
     let len = (textcontent.value).length
+    let rem = 200 -len
     result.textContent = `Total Character: ${len}`
+    remaining.textContent = `Remaining: ${rem}`
+    if(rem == 0){
+        const updateText = document.createElement('p')
+        updateText.classList.add('update')
+        updateText.textContent = `no space`
+        updateText.style.textAlign = "center"
+        document.querySelector('.container').appendChild(updateText)
+    }
+
 })
