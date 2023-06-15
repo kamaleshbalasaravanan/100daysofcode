0e7afee939767f47dd0af581b537b3af8d05ab02
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-15 16:06:28 +0530

    promise fun

--- Random-Joke-Generator/main.js
+++ Random-Joke-Generator/main.js
@@ -1,1 +1,26 @@
-console.log('hi')
+// console.log('hi')
+
+
+const smileicon = document.querySelector('.smile-icon')
+const jokes = document.querySelector('.jokes')
+const ans = document.querySelector('#ans')
+const btn = document.querySelector('#btn')
+
+
+let getJokes =  () =>{
+    fetch('https://v2.jokeapi.dev/joke/Programming')
+    .then((data) => data.json())  
+    .then((content) => {
+        jokes.innerHTML = `${content.setup}`
+        if(content.delivery === 0){
+            ans.innerHTML = "-"
+        }
+        else{
+            ans.innerHTML = `${content.delivery}`
+        }
+
+    })
+
+}
+// console.log(content.setup, content.delivery))
+btn.addEventListener("click", getJokes)
