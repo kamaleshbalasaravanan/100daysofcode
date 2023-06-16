b5a418fd0dbf53e61ad70cf3e6a0131a4b74eb8b
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-16 15:54:14 +0530

    finished script file

--- form-regis-validation/main.js
+++ form-regis-validation/main.js
@@ -14,18 +14,93 @@ console.log(eInput.value == "" ? "yes" : "no")
 console.log(pInput.value == "" ? "yes" : "no")
 console.log(rpInput.value == "" ? "yes" : "no")
 
-form.onsubmit = (e)  => {
+form.onsubmit = (e) => {
     e.preventDefault()
 
-    if(eInput.value == ""){
+    if (eInput.value == "") {
         eField.classList.add('shake', 'error')
     }
-
-    if(pInput.value == "") {
-        pField.classList.add('shake', 'error')
+    else {
+        checkmail()
     }
 
-    if(rpInput.value == "") {
+    if (pInput.value == "") {
+        pField.classList.add('shake', 'error')
+    }
+    else {
+        createPassw()
+    }
+
+    if (rpInput.value == "") {
         rpField.classList.add('shake', 'error')
+    }
+    else{
+        checkpassw()
+    }
+
+    setTimeout(() => {
+        eField.classList.remove('shake')
+        pField.classList.remove('shake')
+        rpField.classList.remove('shake')
+
+    }, 500)
+
+    eInput.onkeyup = () => {
+        checkmail()
+    }
+
+    pInput.onkeyup = () => {
+        createPassw()
+    }
+    rpInput.onkeyup = () => {
+        checkpassw()
+    }
+}
+
+function checkmail() {
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+
+    if (!eInput.value.match(pattern)) {
+        eField.classList.add('error')
+
+        let errortxt = eField.querySelector(".error-txt");
+
+        (eInput.value != "") ? errortxt.innerHTML = " Enter a valid email address" : errortxt.innerHTML = "Email can't be blank"
+    }
+    else {
+        eField.classList.remove('error')
+    }
+}
+
+function createPassw() {
+    // const passPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    const passPattern = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/
+
+    if (!pInput.value.match(passPattern)) {
+        pField.classList.add('error')
+
+        let errortxt = pField.querySelector(".error-txt");
+
+        (pInput.value != "") ? errortxt.innerHTML = "Please enter alteast 8 character with number,symbol,small and capital letter." : errortxt.innerHTML = "Password can't be blank"
+
+    }
+    else {
+        pField.classList.remove('error')
+    }
+}
+
+function checkpassw() {
+    if(pInput.value !== rpInput.value || rpInput === ""){
+        rpField.classList.add('error')
+    }
+    else{
+        if(pInput.value === rpInput.value){
+            rpField.classList.add('error')
+            let errortxt = rpField.querySelector(".error-txt");
+            let errorvalue = pField.querySelector(".error-txt")
+            errortxt.innerHTML = "Password matched"
+            pField.classList.a
+        }
+
     }
 }
