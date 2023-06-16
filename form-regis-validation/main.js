90848a3e959e5df84850fa3e088c8c3a20cc7425
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-16 12:11:49 +0530

    add onsubmit events

--- form-regis-validation/main.js
+++ form-regis-validation/main.js
@@ -1,1 +1,31 @@
-console.log('hi')
+// console.log('hi')
+
+const form = document.querySelector('form')
+const eField = form.querySelector('.email')
+const eInput = eField.querySelector('input')
+
+const pField = form.querySelector('.password')
+const pInput = pField.querySelector('input')
+
+const rpField = form.querySelector('.repassword')
+const rpInput = rpField.querySelector('input')
+
+console.log(eInput.value == "" ? "yes" : "no")
+console.log(pInput.value == "" ? "yes" : "no")
+console.log(rpInput.value == "" ? "yes" : "no")
+
+form.onsubmit = (e)  => {
+    e.preventDefault()
+
+    if(eInput.value == ""){
+        eField.classList.add('shake', 'error')
+    }
+
+    if(pInput.value == "") {
+        pField.classList.add('shake', 'error')
+    }
+
+    if(rpInput.value == "") {
+        rpField.classList.add('shake', 'error')
+    }
+}
