c95155981784f445be19c1394537278b01cd7fee
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-17 20:35:51 +0530

    add genPassword func

--- Random-Password-Generator/main.js
+++ Random-Password-Generator/main.js
@@ -1,1 +1,21 @@
-console.log('hi')
+const password = document.querySelector('#password')
+const copytext = document.querySelector('.copy')
+const btn = document.querySelector('#btn')
+
+btn.addEventListener("click",genPassword)
+
+function genPassword(){
+    console.log('password')
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let passwordlen = 8
+    let password = ""
+
+    for(let i = 0; i<=passwordlen; i++){
+        let randomnum = Math.floor(Math.random() * chars.length)
+        password += chars.substring(randomnum, randomnum+1)
+    }
+
+    // password.value = password
+    document.getElementById("password").value= password
+    
+}
