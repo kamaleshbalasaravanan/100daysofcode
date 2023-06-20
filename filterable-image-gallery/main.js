a14ddbdbbf89878454d3da2c4b4e05dd887dcf04
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-20 10:26:19 +0530

    added eventlistener

--- filterable-image-gallery/main.js
+++ filterable-image-gallery/main.js
@@ -1,1 +1,20 @@
-console.log('hi')
+const filterableButtons = document.querySelectorAll('.filterable-buttons button')
+const filterableCards = document.querySelectorAll('.filterable-cards .cards')
+console.log(filterableButtons)
+console.log(filterableCards)
+
+const filterCards = (e) => {
+    document.querySelector('.active').classList.remove('active')
+    e.target.classList.add('active')
+    // console.log(e.target)
+
+    filterableCards.forEach(card => {
+        card.classList.add('hide')
+
+        if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all"){
+            card.classList.remove('hide')
+        }
+    })
+}
+
+filterableButtons.forEach(button => button.addEventListener("click", filterCards))
