808bef3f9838ef23704918a8affd92f0fb0e4582
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-20 12:28:38 +0530

    addEventlistener

--- tab-section/main.js
+++ tab-section/main.js
@@ -1,1 +1,18 @@
-console.log('hi')
+// console.log('hi')
+const tabs = document.querySelectorAll('.tabs h3')
+const tabContent = document.querySelectorAll('.tab-content div')
+
+console.log(tabs, tabContent)
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener("click" , () => {
+        tabContent.forEach((content) => {
+            content.classList.remove('active')
+        })
+        tabs.forEach((tab) => {
+            tab.classList.remove('active')
+        })
+        tabContent[index].classList.add('active')
+        tabs[index].classList.add('active')
+    })
+})
