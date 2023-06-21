48317c6f36a1323c374c155cbf5f32402b85f717
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-21 22:26:37 +0530

    wincondition

--- Tic-Tac-Toe/main.js
+++ Tic-Tac-Toe/main.js
@@ -1,1 +1,17 @@
-console.log("hi")
+// console.log("hi")
+const cell = document.querySelectorAll('.cell')
+const statusText = document.querySelector('.statusText')
+const restartBtn = document.querySelector('#restartBTn')
+
+const winConditions = [
+    [0,1,2]
+    [3,4,5]
+    [6,7,8]
+
+    [0,3,6]
+    [1,4,7]
+    [2,5,8]
+
+    [0,4,8]
+    [6,4,2]
+]
