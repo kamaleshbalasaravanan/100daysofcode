000f83210f9791571966a2b28c9729dfd91604ae
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-21 13:04:14 +0530

    update commit

--- tab-section/main.js
+++ tab-section/main.js
@@ -1,6 +1,7 @@
 // console.log('hi')
 const tabs = document.querySelectorAll('.tabs h3')
 const tabContent = document.querySelectorAll('.tab-content div')
+const tabcontainer = document.querySelector('.tab-content')
 
 console.log(tabs, tabContent)
 
@@ -8,11 +9,28 @@ tabs.forEach((tab, index) => {
     tab.addEventListener("click" , () => {
         tabContent.forEach((content) => {
             content.classList.remove('active')
+            tabcontainer.classList.add('shake')
         })
         tabs.forEach((tab) => {
             tab.classList.remove('active')
         })
+
+        // tabContent.forEach((content) => {
+        //     tabContent
+        // })
         tabContent[index].classList.add('active')
-        tabs[index].classList.add('active')
+        tabs[index].classList.add('active')  
+        // shake()
+             
     })
 })
+
+// tabs.forEach((tab) => {
+//     tab.addEventListener("click", ()=>{
+//         tabcontainer.classList.add('shake')
+//     })
+// })
+
+// function shake(){
+//     tabcontainer.classList.add('shake')
+// }
