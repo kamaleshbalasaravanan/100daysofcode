88b777bcfca764ced2ea8db05c558b1fccc3e637
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-22 16:20:57 +0530

    final commit

--- Sticky-Navbar/main.js
+++ Sticky-Navbar/main.js
@@ -1,1 +1,12 @@
-console.log('hi')
+// console.log('hi')
+const navbar = document.getElementById('navbar')
+const menu = document.getElementById('menu')
+
+window.onscroll = function(){
+    if(window.pageYOffset >= menu.offsetTop){
+        navbar.classList.add('sticky')
+    }
+    else{
+        navbar.classList.remove('sticky')
+    }
+}
