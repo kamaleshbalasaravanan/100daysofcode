4055d4f2eb39314bbca5bfb2b31da5420296f1c4
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-25 02:18:42 +0530

    get current date

--- Dynamic_Calendaer/main.js
+++ Dynamic_Calendaer/main.js
@@ -1,1 +1,16 @@
-console.log('hi')
+// console.log('hi')
+const currentdate = document.querySelector('.currentdate')
+// console.log(currentdate)
+
+
+let date = new Date
+let currentyear = date.getFullYear()
+let currentmonth = date.getMonth()
+
+let months = ['January','February','March','April','May','June','August','September','October','November','December']
+
+let renderCalender = () => {
+    currentdate.innerHTML = `${months[currentmonth]} ${currentyear}`
+}
+
+renderCalender()
