b0b94bebe8ca15d9e60d62f831373a30df6c7d47
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-25 22:10:07 +0530

    add renderCalender func

--- Dynamic_Calendaer/main.js
+++ Dynamic_Calendaer/main.js
@@ -1,16 +1,36 @@
 // console.log('hi')
 const currentdate = document.querySelector('.currentdate')
+const daysTag = document.querySelector('.days')
 // console.log(currentdate)
+const prevnextIcon = document.querySelectorAll('.icons i')
 
 
 let date = new Date
 let currentyear = date.getFullYear()
 let currentmonth = date.getMonth()
+console.log(currentyear, currentmonth)
 
 let months = ['January','February','March','April','May','June','August','September','October','November','December']
 
 let renderCalender = () => {
+    let lastDateofMonth = new Date(currentyear, currentmonth+1,0).getDate()
+    console.log(lastDateofMonth)
+
+    let liTag =""
+    for(let i=1; i<=lastDateofMonth; i++){
+        // console.log(i)
+        liTag += `<li>${i}</li>`
+    }
     currentdate.innerHTML = `${months[currentmonth]} ${currentyear}`
+    daysTag.innerHTML = liTag
 }
+
+prevnextIcon.forEach(icon =>{
+    icon.addEventListener("click",()=>{
+        // console.log(icon)
+        currentmonth = icon.id == "prev" ? currentmonth-1 : currentmonth+1
+        renderCalender()
+    })
+})
 
 renderCalender()
