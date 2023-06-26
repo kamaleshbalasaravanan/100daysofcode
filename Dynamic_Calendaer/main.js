0083f69ad720a46a8ebc2da6f8d1822c128e856f
kamaleshwaran2002 <ks110302@gmail.com>
2023-06-26 10:45:17 +0530

    add eventlistener for icon

--- Dynamic_Calendaer/main.js
+++ Dynamic_Calendaer/main.js
@@ -13,13 +13,34 @@ console.log(currentyear, currentmonth)
 let months = ['January','February','March','April','May','June','August','September','October','November','December']
 
 let renderCalender = () => {
+
+    let firstdayofMonth = new Date(currentyear, currentmonth,1).getDay()
     let lastDateofMonth = new Date(currentyear, currentmonth+1,0).getDate()
-    console.log(lastDateofMonth)
+    let lastDateofLastMonth = new Date(currentyear, currentmonth,0).getDate()
+    // let firstdarofNextMonth = new Date(currentyear, currentmonth+1, 1).getDay()
+    let lastdayofMonth = new Date(currentyear, currentmonth, lastDateofMonth).getDay()
+    console.log("firstdayofMonth"+firstdayofMonth)
+    console.log("lastDateofMonth"+lastDateofMonth)
+    console.log("lastDateofLastMonth"+lastDateofLastMonth)
+    console.log("lastdayofMonth"+lastdayofMonth)
 
     let liTag =""
+
+    for(let i=firstdayofMonth; i>0; i--){
+        // console.log(i)
+        liTag += `<li class="inactive">${(lastDateofLastMonth+1)-i}</li>`
+    }
+
     for(let i=1; i<=lastDateofMonth; i++){
         // console.log(i)
-        liTag += `<li>${i}</li>`
+        let isToday = i === date.getDate() && currentmonth === date.getMonth() 
+        && currentyear === date.getFullYear() ? "active"  : ""
+
+        liTag += `<li class="${isToday}">${i}</li>`
+    }
+
+    for(let i = lastdayofMonth; i<6; i++){
+        liTag += `<li class="inactive">${i-lastdayofMonth  + 1}</li>`
     }
     currentdate.innerHTML = `${months[currentmonth]} ${currentyear}`
     daysTag.innerHTML = liTag
