f2768b8b51467c5931f5b03dc0da5956a4435ab3
kamaleshwaran2002 <ks110302@gmail.com>
2023-07-09 16:33:02 +0530

    sliding image on mouse move

--- Cards-Slider/main.js
+++ Cards-Slider/main.js
@@ -1,1 +1,31 @@
-console.log('hi')
+// console.log('hi')
+const carousel = document.querySelector('.carousel')
+
+let isdragStart = false
+let startX, startScrollLeft
+const dragStart = (e) => {
+    // console.log("mousedown")
+    isdragStart = true
+    carousel.classList.add('dragging')
+    startX = e.pageX
+    startScrollLeft = carousel.scrollLeft
+    console.log(startX, startScrollLeft)
+}
+
+const dragStop = (e) => {
+    isdragStart = false
+    carousel.classList.remove('dragging')
+    // console.log(e.pageX)
+}
+
+const dragging = (e) =>{
+    // console.log("dragging"+e.pageX)
+    if(!isdragStart) return
+    // console.log("dragStart")
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX)
+}
+
+
+carousel.addEventListener("mousedown", dragStart)
+carousel.addEventListener("mousemove", dragging)
+document.addEventListener("mouseup", dragStop)
