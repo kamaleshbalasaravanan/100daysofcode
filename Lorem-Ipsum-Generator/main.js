8cc4983aaa64b9f54ef632d79dc97638523195da
kamaleshwaran2002 <ks110302@gmail.com>
2023-07-17 19:28:05 +0530

    updated

--- Lorem-Ipsum-Generator/main.js
+++ Lorem-Ipsum-Generator/main.js
@@ -1,19 +1,26 @@
+const generateBtn = document.querySelector('#generateBtn')
 const copyBtn = document.querySelector('#copybtn')
 const paraDisplay = document.querySelector("#para")
-
-const options = {
-    method : "GET",
-    headers: { "x-api-key": apikey },   
+let options = {
+    method: 'GET',
+    // headers: { "X-Api-Key": apikey},   
+    headers: { 'x-api-key': apikey }
 }
 
-const url = "https://api.api-ninjas.com/v1/loremipsum?paragraphs="
+// let url = "https://api.api-ninjas.com/v1/loremipsum?paragraphs="
+let url = "https://api.api-ninjas.com/v1/loremipsum?paragraphs="
 
 let generateParas = () => {
     let noOfPara = document.getElementById("numofPara").ariaValueMax
-    let finalURL = url + noOfPara   
+    let finalURL = url + noOfPara
+    console.log(finalURL)
     fetch(finalURL, options)
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data)
-    })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+        })
 }
+
+generateBtn.addEventListener("click", generateParas)
+window.addEventListener("load", generateParas)
+
