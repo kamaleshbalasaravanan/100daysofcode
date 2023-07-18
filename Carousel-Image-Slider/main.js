b9a934b98cb0ca941d8743a2c5c096a233556bb5
kamaleshwaran2002 <ks110302@gmail.com>
2023-07-18 16:32:11 +0530

    updated

--- Carousel-Image-Slider/main.js
+++ Carousel-Image-Slider/main.js
@@ -1,1 +1,16 @@
-console.log('hi')
+const icons = document.querySelectorAll('.carousel i')
+// console.log(icons)
+ 
+icons.forEach((icon) => {
+    icon.addEventListener("click", () =>{
+        const offset = icon.id == "right" ? 1 : -1
+        // console.log(offset)
+        // const slides = icon.closest("[data-carousel").querySelector("[data-slides")
+        const slides = icon.closest(".carousel").querySelector("ul")
+        // console.log(slides)
+        const activeslide = slides.querySelector("[data-active")
+        // console.log(activeslide)
+        let newIndex = [...slides.children].indexOf(activeslide) + offset
+        console.log(newIndex)
+    }) 
+})
