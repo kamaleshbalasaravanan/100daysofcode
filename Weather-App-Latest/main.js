5133cf0febcb2d1423564044f40083ed31ea43e9
kamaleshwaran2002 <ks110302@gmail.com>
2023-08-15 11:47:46 +0530

    update

--- Weather-App-Latest/main.js
+++ Weather-App-Latest/main.js
@@ -10,15 +10,29 @@ let apikey = "18442d8235523485b0bddc1be2e737d3"
 let api
 
 
-let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-let date = new Date()
-let currentyear = date.getFullYear()
-let currentmonth = date.getMonth()
-console.log(currentyear, currentmonth)
+// let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+// let date = new Date()
+// let currentyear = date.getFullYear()
+// let currentmonth = date.getMonth()
+// console.log(currentyear, currentmonth)
 
-let renderCalender = () => {
+const days = ['Sunday', 'Monday' ,'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC',]
 
-}
+setInterval(() =>{
+    const time = new Date()
+    const month = time.getMonth()
+    const date = time.getDate()
+    const day = time.getDay()
+    const hour = time.getHours()
+    const hoursIn12HrFormat = hour >= 13 ? hour%12 :hour
+    const minutes = "0"+time.getMinutes()
+    const ampm = hour >=12 ?'PM' :'AM'
+
+    document.querySelector("#time").innerHTML = hoursIn12HrFormat + ":" +minutes.substr(-2) + ' '+`<span id="ampm">${ampm}</span></h2>`
+
+    document.querySelector("#date").innerHTML = days[day] +', '+date+' '+months[month]
+},1000)
 
 
 // const date = new Date()
@@ -34,14 +48,14 @@ inputField.addEventListener("keyup", (e) => {
     }
 })
 
-locationBtn.addEventListener("click", () => {
+function callWeather() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(onSuccess, onError)
     }
     else {
         alert("Your browser not support geolocation api")
     }
-})
+}
 
 function onSuccess(position) {
     console.log(position)
@@ -86,6 +100,12 @@ function weatherDetails(info) {
         const { speed } = info.wind
         const { timezone } = info
         let { sunrise, sunset } = info.sys
+        let riseDate = new Date(sunrise * 1000)
+        let riseMin = "0"+riseDate.getMinutes()
+        let setDate = new Date(sunset * 1000)
+        let setMin = "0"+setDate.getMinutes()
+        let riseDateFormat = riseDate.getHours() >= 13? riseDate.getHours()%12 :riseDate.getHours()
+        let setDateFormat = setDate.getHours() >= 13 ? setDate.getHours()%12 :setDate.getHours()
         // console.log(sunrise, sunset)
         // utc = localTime + (sunrise * 60000)
         // const utc = date.getTime() + (date.getTimezoneOffset() * 60)
@@ -124,10 +144,14 @@ function weatherDetails(info) {
         secondContainer.querySelector('.feels-like').innerHTML = `${Math.floor(feels_like)}` + `<span class="deg">&#176;</span>C`
         secondContainer.querySelector('.pressure').innerHTML = Math.floor(pressure) + " hPa"
         secondContainer.querySelector('.wind-speed').innerHTML = Math.floor(speed) + " kph"
-        secondContainer.querySelector('.min-temp').innerHTML = temp_min + `<span class="deg">&#176;</span>C`
-        secondContainer.querySelector('.max-temp').innerHTML = temp_max + `<span class="deg">&#176;</span>C`
+        secondContainer.querySelector('.min-temp').innerHTML ="Min- "+ Math.floor(temp_min) + `<span class="deg">&#176;</span>C`
+        secondContainer.querySelector('.max-temp').innerHTML ="Max- "+ Math.floor(temp_max) + `<span class="deg">&#176;</span>C`
 
-
+        secondContainer.querySelector('#sunrise').innerHTML = `${riseDate.getHours()}`+": "+ riseMin.substr(-2)
+        secondContainer.querySelector('#sunset').innerHTML = setDateFormat+": "+ setMin.substr(-2)
 
     }
 }
+
+locationBtn.addEventListener("click", callWeather)
+window.addEventListener("load",callWeather)
