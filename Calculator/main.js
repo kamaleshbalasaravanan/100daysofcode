752c849051bea532106cf5a0603392368edc6c72
kamaleshwaran2002 <ks110302@gmail.com>
2023-08-20 23:46:23 +0530

    updated

---
+++ Calculator/main.js
@@ -0,0 +1,178 @@
+let currentnum = ""
+let prevnum = ""
+let operator = ""
+
+const currentDisplayNumber = document.querySelector(".currentnum")
+const previousDisplayNumber = document.querySelector(".prevnum")
+
+window.addEventListener("keydown", handleKeyPress)
+
+const equal = document.querySelector(".equalbox")
+const decimal = document.querySelector(".decimal")
+const clear = document.querySelector(".clear")
+
+const numberBtns = document.querySelectorAll('.number')
+const operators = document.querySelectorAll('.operator')
+
+
+equal.addEventListener("click", () => {
+    if (currentnum != "" && prevnum != "") {
+        calculate()
+    }
+})
+clear.addEventListener("click", clearCalculator)
+decimal.addEventListener("click",addDecimal)
+
+numberBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        // e.preventDefault()
+        handleNumber(e.target.value)
+        // console.log(e.target.value)
+    })
+})
+
+function handleNumber(number) {
+    // console.log(number)
+
+    if (currentnum.length <= 11) {
+        currentnum += number
+        currentDisplayNumber.textContent = currentnum
+    }
+}
+
+operators.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        handleOperator(e.target.value)
+    })
+})
+
+function handleOperator(op) {
+
+    if(prevnum !== "" && currentnum !== "" && operator === ""){
+        prevnum = ""
+        currentDisplayNumber.textContent = currentnum
+    }
+
+    if(prevnum === ""){
+        prevnum = currentnum
+        checkOperator(op)
+    }
+    else if(currentnum === ""){
+        checkOperator(op)
+    }
+    else{
+        calculate()
+        operator = op
+        currentDisplayNumber.textContent = "0"
+        previousDisplayNumber.textContent = prevnum + " " +operator
+    }
+}
+
+function checkOperator(text) {
+    operator = text
+    previousDisplayNumber.textContent = prevnum + " " + operator
+    currentDisplayNumber.textContent = "0"
+    currentnum = ""
+}
+
+function calculate() {
+    prevnum = Number(prevnum)
+    currentnum = Number(currentnum)
+
+    if (operator === "+") {
+        prevnum += currentnum
+    }
+    else if (operator === "-") {
+        prevnum -= currentnum
+    }
+
+    else if (operator === "x") {
+        prevnum *= currentnum
+    }
+
+    else if (operator === "/") {
+        if (currentnum <= 0) {
+            prevnum = "Error"
+            displayResults()
+            return
+        }
+
+        prevnum /= currentnum
+    }
+
+    prevnum = roundNum(prevnum)
+    prevnum = prevnum.toString()
+    displayResults()
+}
+
+
+function roundNum(num) {
+    return Math.round(num * 100000) / 100000
+}
+
+
+function displayResults() {
+
+
+    if (prevnum.length <= 11) {
+        currentDisplayNumber.textContent = prevnum
+    }
+    else {
+        currentDisplayNumber.textContent = prevnum.slice(0, 11) + "..."
+    }
+
+    previousDisplayNumber.textContent = ""
+    operator = ""
+    currentnum = ""
+}
+
+function clearCalculator() {
+    currentnum = ""
+    prevnum = ""
+    currentDisplayNumber.textContent = "0"
+    previousDisplayNumber.textContent = ""
+}
+
+function addDecimal(){
+    if(!currentnum.includes(".")){
+        currentnum += "."
+        currentDisplayNumber.textContent = currentnum
+    }
+}
+
+// function handleKeyPress(e) {
+//     e.preventDefault()
+//     console.log(e.key)
+// }
+
+function handleKeyPress(e) {
+    e.preventDefault()
+    if(e.key >= 0 && e.key <= 9){
+        handleNumber(e.key)
+    }
+
+    if(e.key === "Enter" || e.key === "=" && currentnum != "" && prevnum != ""){
+        compute()
+    }
+
+    if(e.key === "+" || e.key === "-" || e.key === "/"){
+        handleOperator(e.key)
+    }
+
+    if(e.key === "*"){
+        handleOperator("x")
+    }
+
+    if(e.key === "."){
+        addDecimal()
+    }
+
+    if(e.key === "Backspace"){
+        handleDelete()
+    }
+    console.log(e.key   )
+}
+
+function handleDelete(){
+    
+}
