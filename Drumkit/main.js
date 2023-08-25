307cd1976acb827235a8b3903249c2d10ef8be1d
kamaleshwaran2002 <ks110302@gmail.com>
2023-08-26 00:29:37 +0530

    initial commit

--- Drumkit/main.js
+++ Drumkit/main.js
@@ -2,46 +2,55 @@ const nodofDrumButtons = document.querySelectorAll(".drum").length
 const drumButtons = document.querySelectorAll(".drum")
 console.log(drumButtons)
 
-for(let i=0; i<nodofDrumButtons; i++){
-       const val = document.querySelectorAll(".drum")[i].innerHTML
-    console.log(val)
+// for(let i=0; i<nodofDrumButtons; i++){
+//        const val = document.querySelectorAll(".drum")[i].innerHTML
+//     console.log(val)
 
 
-        document.querySelectorAll(".drum")[i].addEventListener("click", (e)=> {
-            let buttonInnerHtml = this.innerHTML
-            console.log(this.innerHTML)
-            console.log(buttonInnerHtml)
-            // console.log('hi')
-            makeSound(buttonInnerHtml)
-            buttonAnimation(buttonInnerHtml)
-        })
+//         document.querySelectorAll(".drum")[i].addEventListener("click", (e)=> {
+//             let buttonInnerHtml = this.innerHTML
+//             console.log(this.innerHTML)
+//             console.log(buttonInnerHtml)
+//             // console.log('hi')
+//             makeSound(buttonInnerHtml)
+//             buttonAnimation(buttonInnerHtml)
+//         })
 
-        // drumButtons.forEach((drum) => {
-        //     drum.addEventListener("click", () => {
-        //         console.log(this)
-        //     })
-        // })
+//         // drumButtons.forEach((drum) => {
+//         //     drum.addEventListener("click", () => {
+//         //         console.log(this)
+//         //     })
+//         // })
+// }
 
 
+for (let i = 0; i < drumButtons.length; i++) {
+    const button = drumButtons[i]
+    const buttonInnerHtml = button.innerHTML
+    console.log(buttonInnerHtml)
+
+    button.addEventListener("click", () => {
+        makeSound(buttonInnerHtml)
+        buttonAnimation(buttonInnerHtml)
+    })
 }
 
-
-document.addEventListener("keypress",(e) => {
+document.addEventListener("keypress", (e) => {
     makeSound(e.key)
     buttonAnimation(e.key)
 })
 
 
-function makeSound(key){
+function makeSound(key) {
 
     // console.log('makesound')
 
-    switch(key) {
+    switch (key) {
         case "w":
             let tom1 = new Audio("sounds/tom-1.mp3")
             tom1.play()
             break;
-        
+
         case "a":
             let tom2 = new Audio("sounds/tom-2.mp3")
             tom2.play()
@@ -52,7 +61,7 @@ function makeSound(key){
             tom3.play()
             break;
 
-        
+
         case "d":
             let tom4 = new Audio("sounds/tom-4.mp3")
             tom4.play()
@@ -73,16 +82,14 @@ function makeSound(key){
             kick.play()
             break;
 
-        default :
+        default:
             console.log(key)
     }
 }
 
 
-function buttonAnimation (currentKey){
-    // console.log('buttonanimation')
-    let activeButton = document.querySelector("."+currentKey)
-    // activeButton.classList.add("pressed")
+function buttonAnimation(currentKey) {
+    let activeButton = document.querySelector("." + currentKey)
     activeButton.classList.add("pressed")
 
     setTimeout(() => {
